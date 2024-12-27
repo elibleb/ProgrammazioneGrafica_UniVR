4b1b90fb7cbf200e9f82869b489217395e6de492
elibleb <127298351+elibleb@users.noreply.github.com>
2024-12-27 13:14:07 +0100

    Add files via upload

--- 1_5.6.cpp
+++ 1_5.6.cpp
@@ -1,4 +1,4 @@
-#include <glad/glad.h> // includi file header di GLAD e GLFW
+ï»¿#include <glad/glad.h> // includi file header di GLAD e GLFW
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -10,29 +10,35 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+//Shader sono programmi che eseguono calcoli sulla GPU
 //shaders
+//Il vertex shader prende le coordinate dei vertici in ingresso e calcola la posizione finale dei vertici 
+//nello spazio della finestra
 const char* vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+"layout (location = 0) in vec3 aPos;\n" //aPos Ã¨ l'attributo che rappresenta la posizione di ciascun vertice
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//Il fragment shader imposta il colore del frammento (pixel) al valore arancione (vec4(1.0f, 0.5f, 0.2f, 1.0f)),
+//che Ã¨ la componente di colore in RGBA
 const char* fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 int main()
 {
     // GLFW: inizializzazione e configurazione
-    
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Versione major e minor 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Profilo principale, 
-                                                             // sottoinsieme più piccolo di funzionalità
+    // sottoinsieme piÃ¹ piccolo di funzionalitÃ 
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -40,7 +46,7 @@ int main()
 
     // GLFW: creazione finestra
     // restituisce un oggetto GLFWwidow
-    
+
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -49,12 +55,12 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window); // rendiamo il contesto della nostra finestra il contesto principale
-                                    // del thread corrente
+    // del thread corrente
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // chiamiamo questa funzione ad 
-                                                                //ogni ridimensionamento della finestra
+    //ogni ridimensionamento della finestra
 
-    // GLAD: carica tutti i puntatori alle funzioni OpenGL
-    
+// GLAD: carica tutti i puntatori alle funzioni OpenGL
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -66,6 +72,7 @@ int main()
     //--------------------------------------
 
     //vertex shader
+    //Viene creato il vertex shader e compilato
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
@@ -80,7 +87,8 @@ int main()
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-     // fragment shader
+    // fragment shader
+    //Viene creato il fragment shader e compilato
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
@@ -94,6 +102,8 @@ int main()
     }
 
     // collegare gli shader (link)
+    // Viene creato un programma shader, che Ã¨ il punto di collegamento per gli shader di vertici e frammenti. 
+    // Dopo averli attaccati al programma, viene eseguito il linking
     unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
@@ -106,27 +116,33 @@ int main()
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
 
-    //eliminare gli oggetti shader una volta collegati al programma in oggetto (non ne abbiamo più bisogno)
+    //eliminare gli oggetti shader una volta collegati al programma in oggetto (non ne abbiamo piÃ¹ bisogno)
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
 
     //-------------------------------------------------------------------------------
     //impostare i dati dei vertici (e i buffer) e configurare gli attributi dei vertici
-     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
+    //viene definito un array di vertici che rappresentano un quadrato, diviso in due triangoli
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f   // top left 
     };
 
-    // VBO = Vertex Buffer Object
-    // VAO = Vertex Array Object
-    // EBO = Element Buffer Object
+    //Viene creato anche un array di indici che descrive l'ordine con cui i vertici 
+    //devono essere usati per disegnare il quadrato. 
+    //Questo Ã¨ utile per ottimizzare il disegno degli oggetti, evitando di duplicare i vertici.
+    unsigned int indices[] = {  //Inizia da zero
+        0, 1, 3,  // Primo Triangle
+        1, 2, 3   // Secondo Triangle
+    };
+
+    // VBO = Vertex Buffer Object, contiene i dati dei vertici
+    // VAO = Vertex Array Object, contiene lo stato degli attributi dei vertici.
+    // EBO = Element Buffer Object, contiene gli indici per i vertici, 
+    // che consentono di ottimizzare il rendering evitando la duplicazione di vertici comuni
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -143,45 +159,44 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // adesso si può fare l'unbind, la chiamata a glVertexAttribPointer ha registrato VBO come oggetto buffer 
+    // adesso si puÃ² fare l'unbind, la chiamata a glVertexAttribPointer ha registrato VBO come oggetto buffer 
     // di vertice associato dell'attributo vertex, quindi possiamo separare tranquillamente
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
 
-    // Questa linea serve per disegnare i poligoni in wireframe.
+    // Questa linea serve per disegnare i poligoni in wireframe
+    // Cambia la modalitÃ  di rendering a wireframe (disegno a linee), utile per visualizzare la geometria dell'oggetto
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
     //------------------------------------------------
     // CICLO DI RENDERING
     // -----------------------------------------------
-    // continua finchè non gli viene detto di chiudere
+    // continua finchÃ¨ non gli viene detto di chiudere
 
     while (!glfwWindowShouldClose(window))
     {
         // input
-        
-        processInput(window);
 
-        // GLFW: scambia i buffer e interroga gli eventi IO (tasti premuti, movimento del mouse etc.)
+        processInput(window); //gestisce l'input
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // funzione che setta lo stato
-        glClear(GL_COLOR_BUFFER_BIT); // funzione che utilizza lo stato
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //colore sfondo
+        glClear(GL_COLOR_BUFFER_BIT); // pulisce il buffer
 
-        //disegna il triangolo
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); //avendo un solo VAO non è necessario bindarlo,  lo teniamo per una 
+        ////Usa il programma shader e disegna l'oggetto (il triangolo)
+        glUseProgram(shaderProgram); 
+        glBindVertexArray(VAO); //avendo un solo VAO non Ã¨ necessario bindarlo,  lo teniamo per una 
         //migliore organizzazione
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //Disegna il quadrato con gli indici, 6 indici, 3 per ogni triangolo
 
         // GLFW: scambia i buffer e interroga gli eventi IO (tasti premuti, movimento del mouse etc.)
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        glfwSwapBuffers(window); //scambia i buffer
+        glfwPollEvents(); //gestisce gli eventi
     }
 
-    // de-alloco le risorse non più necessarie. Non è necessario
+    // de-alloco le risorse non piÃ¹ necessarie. Non Ã¨ necessario
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
@@ -201,7 +216,7 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: ogni volta che la finestra viene modificata (dall'utente o dal SO), viene eseguita 
+// GLFW: ogni volta che la finestra viene modificata (dall'utente o dal SO), viene eseguita 
 // questa funzione di callback (listener del resize della finestra)
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
