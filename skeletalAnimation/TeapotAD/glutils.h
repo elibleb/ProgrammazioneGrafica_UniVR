7ba9b03895abf5a306539807c3ea91f3e3662ffa
elibleb <127298351+elibleb@users.noreply.github.com>
2025-02-02 12:16:40 +0100

    Update glutils.h

--- skeletalAnimation/TeapotAD/glutils.h
+++ skeletalAnimation/TeapotAD/glutils.h
@@ -1,6 +1,9 @@
 #ifndef GLUTILS_H
 #define GLUTILS_H
 
+//codice utilizzato per il debug, funzioni utili per rilevare e stampare gli errori e per ottenere informazioni sull'installazione
+//di OpenGl e le relative estensioni
+
 class GLUtils
 {
 public:
